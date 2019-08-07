29127c931340d51e7d5f80339a293fafff9d97b7
Andrew Semple <asemple@atlassian.com>
2019-08-07 14:36:59 +1000

    Change the verbosity of stow to indicate enough of what we are doing

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ echo Installing dotfiles to $HOME
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RELATIVE_HOME=$(realpath --relative-to=${DIR} ${HOME})
 
-stow -vvvvv --target=${RELATIVE_HOME} bash
+stow -vv --target=${RELATIVE_HOME} bash
+stow -vv --target=${RELATIVE_HOME} git
